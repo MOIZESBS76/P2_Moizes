a17cd1022606c219a61640de2fba90e872dece60
MOIZESBS76 <167489585+MOIZESBS76@users.noreply.github.com>
2024-06-14 20:59:05 -0300

    Subir arquivos

---
+++ script.js
@@ -0,0 +1,10 @@
+const imagem = document.getElementById('imgfacul');
+imagem.addEventListener('click', () => {
+    alert('Imagem clicada!');
+});
+
+const botao = document.getElementById("button");
+botao.addEventListener('click', () => {
+    alert('Obrigado por visitar o site')
+});
+
